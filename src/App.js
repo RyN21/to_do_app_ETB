cc2edc81646992a82373a0e030896cf97eabedee
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-20 22:26:28 -0700

    Understanding basics of React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import TaskList from './TaskList';
 
 function App() {
+  const tasks = [
+    {id: 0, description: 'do this', done: false},
+    {id: 1, description: 'do that', done: false},
+  ];
   return (
     <div>
+      <TaskList />
     </div>
   );
 }
